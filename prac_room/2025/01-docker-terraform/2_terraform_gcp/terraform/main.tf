7ef4ef1e5d7697bc86e698c5e2c09e390248bc0c
ktzhu <1024812321@qq.com>
2025-02-02 06:02:13 +0000

    updated tf files

--- prac_room/2025/01-docker-terraform/2_terraform_gcp/terraform/main.tf
+++ prac_room/2025/01-docker-terraform/2_terraform_gcp/terraform/main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "dtc-de-course-449111"
-  region  = "australia-southeast2"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "dtc-de-course-449111"
-  location      = "australia-southeast2"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +26,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
